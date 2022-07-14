5e05a81b7f5e9b5d34ea24addabec31cb1a3d389
Sophie Miller <millersg47@gmail.com>
2022-07-13 18:33:08 -0700

    fixes icon bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -45,7 +45,7 @@ export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction("jate", "readonly");
+  const tx = jateDb.transaction("jate", "readwrite");
 
   // Open up the desired object store.
   const store = tx.objectStore("jate");
